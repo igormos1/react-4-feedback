cec5314b32602c91f861ddac32640084ff43e958
igormos1 <gior_gt@ukr.net>
2022-04-22 14:22:19 +0300

    igormos1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,47 +1,44 @@
 
-import React,{ useState } from 'react';
-import Statistics from './Statistics';
-import Section from './Section';
-import Notification from './Notification/Notification';
-import FeedbackOptions from './Feedback/Feedback'
+import React, { useState } from 'react';
+import Statistics from "./Statistics";
+import FeedbackOptions from "./Feedback";
+import Section from "./Section";
+import Notification from "./Notification";
 
-const App = ()=>{
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+const App = () => { 
 
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
+  const setEstimate = (estimate) => {
+    switch (estimate) {
+      case "good":
+        setGood(good + 1);
+        break;
+      case "neutral":
+        setNeutral(neutral + 1);
+        break;
+      case "bad":
+        setBad(bad + 1);
+        break;
+      default:
+        alert("Something is wrong");
+    }
+  }   
 
+  const countFeedback = good + neutral +bad;
 
-     const setEstimate = (estimate) => {
-       switch (estimate) {
-         case "good":
-           setGood(good + 1);
-           break;
-         case "neutral":
-           setNeutral(neutral + 1);
-           break;
-         case "bad":
-           setBad(bad + 1);
-           break;
-         default:
-           alert("Something is wrong");
-        } 
-  } 
+  const feedbacks = {good, neutral, bad};
 
-
-  const countFeedback = good + neutral + bad;
-  const feedbacks = { good, neutral, bad };
- 
-      
-      return(
+  return(
           <>
             <Section text='Please leave feed back'>
               <FeedbackOptions 
                     feedbacks = {feedbacks} 
                     onLeaveFeedback={setEstimate}/>
             </Section>
-            { countFeedback()?
+            { countFeedback?
             <Section text='Statistics'>              
               <Statistics feedbacks = {feedbacks}  />
             </Section>:
@@ -50,6 +47,9 @@ const App = ()=>{
             </>
       )
       
-  }
+      
+      
+  
+}
 
 export default App;
